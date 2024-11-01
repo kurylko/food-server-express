b5f1ba9139d17568a8ea35f9228ecd79871de0a7
kurylko <reviliya@gmail.com>
2024-11-01 12:52:55 +0100

    src folder

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 const express = require("express");
 
 const app = express();
-const port = 5000;
+
+require("dotenv").config();
+
+const port = parseInt(process.env.EXPRESS_PORT ?? "5000", 10);
 
 const routes = require('express').Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 const welcome = (req, res) => {
-    res.send("Welcome to server");
+    res.send("Welcome to food server");
 };
 
 app.get("/", welcome);
+app.get("/api", welcome);
 
 app.listen(port, (err) => {
     if (err) {
